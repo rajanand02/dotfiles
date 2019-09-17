f5b8698a70269413c62f263be87b426e8334df18
Raj Anand <rajanand0202@gmail.com>
2019-09-17 23:44:56 +0530

    Delete private-aliases.zsh

--- zsh-config/private-aliases.zsh
+++
@@ -1,6 +0,0 @@
-alias vpn="sudo openvpn ~/Downloads/rajanand@cleartax.in-client.ovpn"
-# work
-alias cdgst='cd ~/cleartax/projects/gst-next'
-
-export PIVOTAL_PROJECT_ID=2241977
-export PIVOTAL_TOKEN=bbf7ba228e4276d8eda0ce849e1fe4ab
